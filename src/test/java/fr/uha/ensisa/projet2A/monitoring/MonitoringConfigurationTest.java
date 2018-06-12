8e334e3564b6686cd5de03370710823b11e50ed1
Axel <axel.huynh-phuc@uha.fr>
2018-06-12 11:19:18 +0200

    Fix a bug into MonitoringConfiguration test

--- src/test/java/fr/uha/ensisa/projet2A/monitoring/MonitoringConfigurationTest.java
+++ src/test/java/fr/uha/ensisa/projet2A/monitoring/MonitoringConfigurationTest.java
@@ -9,104 +9,98 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MonitoringConfigurationTest {
-	
-	private MonitoringConfiguration sut; 
-	
-	@Before 
+
+	private MonitoringConfiguration sut;
+
+	@Before
 	public void create() {
-		String[] IPs = { "demeterIP","haas1IP","haas2IP","haas3IP", }; 
-		String[] machineNames = { "Demeter" , "HAAS_VF2_5AXES" , "HAAS_VF2_3AXES" , "HAAS_SL20" }; 
-		sut = new MonitoringConfiguration("clusterName","localhost",9200,"bdd", IPs , machineNames,8080,5); 
+		String[] IPs = { "demeterIP", "haas1IP", "haas2IP", "haas3IP", };
+		String[] machineNames = { "Demeter", "HAAS_VF2_5AXES", "HAAS_VF2_3AXES", "HAAS_SL20" };
+		sut = new MonitoringConfiguration("clusterName", "localhost", 9200, "bdd", IPs, machineNames, 8080, 5);
 	}
-	
+
 	@Test
 	public void setClusterName() {
 		assertEquals(sut.getClusterNameES(), "clusterName");
 		sut.setClusterNameES("new cluster name");
 		assertEquals(sut.getClusterNameES(), "new cluster name");
 	}
-	
+
 	@Test
 	public void setHostES() {
 		assertEquals(sut.getHostES(), "localhost");
 		sut.setHostES("new host");
 		assertEquals(sut.getHostES(), "new host");
 	}
-	
+
 	@Test
 	public void setPortES() {
 		assertEquals(sut.getPortES(), 9200);
 		sut.setPortES(8080);
 		assertEquals(sut.getPortES(), 8080);
 	}
-	
+
 	@Test
 	public void setSQLHost() {
 		assertEquals(sut.getHostDMGSQL(), "bdd");
 		sut.setHostDMGSQL("new bdd");
-		assertEquals(sut.getHostDMGSQL(),"new bdd");
+		assertEquals(sut.getHostDMGSQL(), "new bdd");
 	}
-	
-	@Test 
+
+	@Test
 	public void setIPs() {
-		String[] IPs = { "demeterIP","haas1IP","haas2IP","haas3IP", }; 
-		String[] newIPs = { "IP1", "IP2" }; 
-		for(int i = 0 ; i < sut.getIPs().length ; i++) {
+		String[] IPs = { "demeterIP", "haas1IP", "haas2IP", "haas3IP", };
+		String[] newIPs = { "IP1", "IP2" };
+		for (int i = 0; i < sut.getIPs().length; i++) {
 			assertEquals(sut.getIPs()[i], IPs[i]);
 		}
 		sut.setIPs(newIPs);
-		for(int i = 0 ; i < sut.getIPs().length ; i++) {
+		for (int i = 0; i < sut.getIPs().length; i++) {
 			assertEquals(sut.getIPs()[i], newIPs[i]);
 		}
 	}
-	
+
 	@Test
 	public void setMoxaPort() {
 		assertEquals(sut.getMoxaPort(), 8080);
 		sut.setMoxaPort(3636);
-		assertEquals(sut.getMoxaPort(),3636);
+		assertEquals(sut.getMoxaPort(), 3636);
 	}
-	
+
 	@Test
 	public void show() {
-		String printed = "**** Monitoring configuration : ****\n" + 
-				"Elasticsearch cluster name = clusterName\n" + 
-				"Elasticsearch host = localhost\n" + 
-				"Elasticsearch port = 9200\n" + 
-				"DMG SQL server host  = bdd\n" + 
-				"Machine : Demeter , IP = demeterIP\n" + 
-				"Machine : HAAS_VF2_5AXES , IP = haas1IP\n" + 
-				"Machine : HAAS_VF2_3AXES , IP = haas2IP\n" + 
-				"Machine : HAAS_SL20 , IP = haas3IP\n" + 
-				"Moxa port = 8080\n" + 
-				"Pooling period = 5\n"+
-				"**** End of configuration ****"; 
-		
+		String printed = "**** Monitoring configuration : ****\n" + "Elasticsearch cluster name = clusterName\n"
+				+ "Elasticsearch host = localhost\n" + "Elasticsearch port = 9200\n" + "DMG SQL server host  = bdd\n"
+				+ "Machine : Demeter , IP = demeterIP\n" + "Machine : HAAS_VF2_5AXES , IP = haas1IP\n"
+				+ "Machine : HAAS_VF2_3AXES , IP = haas2IP\n" + "Machine : HAAS_SL20 , IP = haas3IP\n"
+				+ "Moxa port = 8080\n" + "Pooling period = 5\n" + "**** End of configuration ****";
+
 		assertEquals(printed, sut.toString());
 	}
-	
+
 	@Test
 	public void configByfile() throws FileNotFoundException {
 		
-		sut  = new MonitoringConfiguration("src\\test\\resources\\configTest.txt");
-		String machineNames[] = { "machine1" , "machine2" }; 
-		String IPs[] = { "IP1" , "IP2" }; 
+		sut = new MonitoringConfiguration("../configTest.txt");
 		
+		String machineNames[] = { "machine1", "machine2" };
+		String IPs[] = { "IP1", "IP2" };
+
 		assertEquals(sut.getClusterNameES(), "elasticsearch");
 		assertEquals(sut.getHostES(), "localhost");
 		assertEquals(sut.getPortES(), 9300);
 		assertEquals(sut.getHostDMGSQL(), "bdd");
-		for(int i = 0 ; i < machineNames.length ; i++) {
-			assertEquals(sut.getMachineNames()[i], "machine"+(i+1));
+		for (int i = 0; i < machineNames.length; i++) {
+			assertEquals(sut.getMachineNames()[i], "machine" + (i + 1));
 			assertEquals(sut.getIPs()[i], IPs[i]);
 		}
 		assertEquals(sut.getMoxaPort(), 8080);
 		assertEquals(sut.getPoolingPeriod(), 5);
 	}
-	
-	@Test(expected= java.lang.NullPointerException.class)
-	public void configByNullFile() throws FileNotFoundException  {
-		sut = new MonitoringConfiguration(null); 
+
+	@Test(expected = java.lang.NullPointerException.class)
+	public void configByNullFile() throws FileNotFoundException {
+		sut = new MonitoringConfiguration(null);
 	}
-	
+
 }
